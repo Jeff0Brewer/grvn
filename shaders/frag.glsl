1276f2eb125aaea81e6d8b4888ed0b6a89f48bdf
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-05-25 11:06:28 -0600

    remove unused varying from vert

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -4,10 +4,8 @@ varying vec4 v_Color;
 varying float v_Visibility;
 
 void main() {
-    if(v_Visibility == 0.0) {
-        gl_FragColor = v_Color;
-    }
-    else {
+    if (v_Visibility != 0.0) {
         discard;
     }
+    gl_FragColor = v_Color;
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -6,13 +6,11 @@ uniform mat4 u_ModelMatrix;
 uniform mat4 u_ViewMatrix;
 uniform mat4 u_ProjMatrix;
 
-varying vec3 v_Position;
 varying vec4 v_Color;
 varying float v_Visibility;
 
 void main() {
-    gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
-    v_Position = vec3(u_ModelMatrix * a_Position);
-    v_Visibility = a_Visibility;
     v_Color = a_Color;
+    v_Visibility = a_Visibility;
+    gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
 }
