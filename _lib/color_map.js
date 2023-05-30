f848cebcde6cc2f8ecad78af70fa81198c64c65f
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-05-30 16:46:38 -0600

    format color map

--- _lib/color_map.js
+++ _lib/color_map.js
@@ -1,42 +1,44 @@
-class ColorMap{
-	constructor(css_def){
-		this.steps = [];
-		this.colors = [];
+class ColorMap {
+    constructor (css_def) {
+        this.steps = []
+        this.colors = []
 
-		let curr_ind = 0;
-		while(true){
-			let a = css_def.indexOf('#', curr_ind);
-			let b = css_def.indexOf(' ', curr_ind);
-			let c = css_def.indexOf('%', curr_ind);
-			curr_ind = c + 3;
-			if(a < 0 || b < 0 || c < 0)
-				break;
-			this.colors.push(hex_to_rgb(css_def.substring(a, b)));
-			this.steps.push(parseFloat(css_def.substring(b + 1, c))/100);
-		}
-	}
+        let curr_ind = 0
+        while (true) {
+            const a = css_def.indexOf('#', curr_ind)
+            const b = css_def.indexOf(' ', curr_ind)
+            const c = css_def.indexOf('%', curr_ind)
+            curr_ind = c + 3
+            if (a < 0 || b < 0 || c < 0) { break }
+            this.colors.push(hex_to_rgb(css_def.substring(a, b)))
+            this.steps.push(parseFloat(css_def.substring(b + 1, c)) / 100)
+        }
+    }
 
-	map(value, low, high){
-		if(value <= low){
-			return this.colors[0];
-		}
-		if(value >= high){
-			return this.colors[this.colors.length - 1];
-		}
+    map (value, low, high) {
+        if (value <= low) {
+            return this.colors[0]
+        }
+        if (value >= high) {
+            return this.colors[this.colors.length - 1]
+        }
 
-		let percentage = (value - low)/(high - low);
-		let i = 0;
-		while(this.steps[i] < percentage)
-			i++;
-		if(i + 1 == this.steps.length)
-			return this.colors[i];
-		if(this.steps[i] == this.steps[i + 1])
-			i++
-		let mid = (percentage - this.steps[i])/(this.steps[i + 1] - this.steps[i]);
-		return {
-			r: this.colors[i].r*(1 - mid) + this.colors[i + 1].r*mid,
-			g: this.colors[i].g*(1 - mid) + this.colors[i + 1].g*mid,
-			b: this.colors[i].b*(1 - mid) + this.colors[i + 1].b*mid,
-		};
-	}
+        const percentage = (value - low) / (high - low)
+        let i = 0
+        while (this.steps[i] < percentage) {
+            i++
+        }
+        if (i + 1 === this.steps.length) {
+            return this.colors[i]
+        }
+        if (this.steps[i] === this.steps[i + 1]) {
+            i++
+        }
+        const mid = (percentage - this.steps[i]) / (this.steps[i + 1] - this.steps[i])
+        return {
+            r: this.colors[i].r * (1 - mid) + this.colors[i + 1].r * mid,
+            g: this.colors[i].g * (1 - mid) + this.colors[i + 1].g * mid,
+            b: this.colors[i].b * (1 - mid) + this.colors[i + 1].b * mid
+        }
+    }
 }
--- _lib/color_map_slider.js
+++ _lib/color_map_slider.js
@@ -1,214 +1,208 @@
-class ColorMapSlider{
-	constructor(css_def){
-		this.elements = {
-			bar: document.getElementById('cm_bar'),
-			bars: {
-				low: document.getElementById('cm_low'),
-				gradient: document.getElementById('cm_g'),
-				high: document.getElementById('cm_high')
-			},
-			handles: {
-				low: document.getElementById('cm_h_low'),
-				high: document.getElementById('cm_h_high'),
-			},
-			labels: {
-				min: document.getElementById('cm_min'),
-				max: document.getElementById('cm_max'),
-				low: document.getElementById('cm_low_val'),
-				high: document.getElementById('cm_high_val'),
-			},
-			hover: {
-				section: document.getElementById('cm_hover_section'),
-				line: document.getElementById('cm_hover_line'),
-				label: document.getElementById('cm_hover_label')
-			},
-			button: document.getElementById('edit_color_map')
-		};
+class ColorMapSlider {
+    constructor (css_def) {
+        this.elements = {
+            bar: document.getElementById('cm_bar'),
+            bars: {
+                low: document.getElementById('cm_low'),
+                gradient: document.getElementById('cm_g'),
+                high: document.getElementById('cm_high')
+            },
+            handles: {
+                low: document.getElementById('cm_h_low'),
+                high: document.getElementById('cm_h_high')
+            },
+            labels: {
+                min: document.getElementById('cm_min'),
+                max: document.getElementById('cm_max'),
+                low: document.getElementById('cm_low_val'),
+                high: document.getElementById('cm_high_val')
+            },
+            hover: {
+                section: document.getElementById('cm_hover_section'),
+                line: document.getElementById('cm_hover_line'),
+                label: document.getElementById('cm_hover_label')
+            },
+            button: document.getElementById('edit_color_map')
+        }
 
-		this.dragging = {
-			left: false,
-			right: false
-		};
+        this.dragging = {
+            left: false,
+            right: false
+        }
 
-		this.map = new ColorMap(css_def);
+        this.map = new ColorMap(css_def)
 
-		css_def = 'linear-gradient(90deg,' + css_def + ')';
+        css_def = 'linear-gradient(90deg,' + css_def + ')'
 
-		let body_rect = this.elements.bar.getBoundingClientRect();
+        const body_rect = this.elements.bar.getBoundingClientRect()
 
-		this.elements.bars.low.style.width = '0px';
-		this.elements.bars.gradient.style.width = body_rect.width.toString() + 'px';
-		this.elements.bars.high.style.width = '0px';
+        this.elements.bars.low.style.width = '0px'
+        this.elements.bars.gradient.style.width = body_rect.width.toString() + 'px'
+        this.elements.bars.high.style.width = '0px'
 
-		this.elements.bars.gradient.style.background = css_def;
-		this.elements.bars.low.style.background = 'rgb(' + (this.map.colors[0].r*255).toString() + ',' + (this.map.colors[0].g*255).toString() + ',' + (this.map.colors[0].b*255).toString() + ')';
-		this.elements.bars.high.style.background = 'rgb(' + (this.map.colors[this.map.colors.length - 1].r*255).toString() + ',' + (this.map.colors[this.map.colors.length - 1].g*255).toString() + ',' + (this.map.colors[this.map.colors.length - 1].b*255).toString() + ')';
-	
-		this.values = {
-			low: 0,
-			high: 1,
-			min: 0,
-			max: 1
-		};
+        this.elements.bars.gradient.style.background = css_def
+        this.elements.bars.low.style.background = 'rgb(' + (this.map.colors[0].r * 255).toString() + ',' + (this.map.colors[0].g * 255).toString() + ',' + (this.map.colors[0].b * 255).toString() + ')'
+        this.elements.bars.high.style.background = 'rgb(' + (this.map.colors[this.map.colors.length - 1].r * 255).toString() + ',' + (this.map.colors[this.map.colors.length - 1].g * 255).toString() + ',' + (this.map.colors[this.map.colors.length - 1].b * 255).toString() + ')'
 
-		this.elements.labels.min.innerHTML = 0;
-		this.elements.labels.max.innerHTML = 1;
-		this.elements.labels.low.innerHTML = 0;
-		this.elements.labels.high.innerHTML = 1;
+        this.values = {
+            low: 0,
+            high: 1,
+            min: 0,
+            max: 1
+        }
 
-		this.data = [];
-	}
+        this.elements.labels.min.innerHTML = 0
+        this.elements.labels.max.innerHTML = 1
+        this.elements.labels.low.innerHTML = 0
+        this.elements.labels.high.innerHTML = 1
 
-	change_data(str_data, num_g){
-		let lines = str_data.split('\n');
-		lines.pop();
-		let num_t = lines.length/num_g;
-		this.data = [];
-		for(let t = 0; t < num_t; t++){
-			this.data.push([]);
-			for(let g = 0; g < num_g; g++){
-				this.data[t].push(0);
-			}
-		}
-		let t = num_t - 1;
-		let g = num_g - 1;
-		this.values.max = -10000000;
-		this.values.min = 10000000;
-		while(lines.length > 0){
-			let val = parseFloat(lines.pop());
-			this.values.max = max(this.values.max, val);
-			this.values.min = min(this.values.min, val);
-			this.data[t][g] = val;
-			g--;
-			if(g < 0){
-				g = num_g - 1;
-				t--;
-			}
-		}
-		this.values.low = this.values.min;
-		this.values.high = this.values.max;
+        this.data = []
+    }
 
-		this.elements.labels.min.innerHTML = this.values.min.toFixed(2);
-		this.elements.labels.max.innerHTML = this.values.max.toFixed(2);
-		this.elements.labels.low.innerHTML = this.values.min.toFixed(2);
-		this.elements.labels.high.innerHTML = this.values.max.toFixed(2);
+    change_data (str_data, num_g) {
+        const lines = str_data.split('\n')
+        lines.pop()
+        const num_t = lines.length / num_g
+        this.data = []
+        for (let t = 0; t < num_t; t++) {
+            this.data.push([])
+            for (let g = 0; g < num_g; g++) {
+                this.data[t].push(0)
+            }
+        }
+        let t = num_t - 1
+        let g = num_g - 1
+        this.values.max = -10000000
+        this.values.min = 10000000
+        while (lines.length > 0) {
+            const val = parseFloat(lines.pop())
+            this.values.max = max(this.values.max, val)
+            this.values.min = min(this.values.min, val)
+            this.data[t][g] = val
+            g--
+            if (g < 0) {
+                g = num_g - 1
+                t--
+            }
+        }
+        this.values.low = this.values.min
+        this.values.high = this.values.max
 
-		let body_rect = this.elements.bar.getBoundingClientRect();
+        this.elements.labels.min.innerHTML = this.values.min.toFixed(2)
+        this.elements.labels.max.innerHTML = this.values.max.toFixed(2)
+        this.elements.labels.low.innerHTML = this.values.min.toFixed(2)
+        this.elements.labels.high.innerHTML = this.values.max.toFixed(2)
 
-		this.elements.bars.low.style.width = '0px';
-		this.elements.bars.gradient.style.width = body_rect.width.toString() + 'px';
-		this.elements.bars.high.style.width = '0px';
+        const body_rect = this.elements.bar.getBoundingClientRect()
 
-		this.elements.labels.low.style.marginLeft = '0px';
-		this.elements.labels.high.style.marginRight = '0px';
+        this.elements.bars.low.style.width = '0px'
+        this.elements.bars.gradient.style.width = body_rect.width.toString() + 'px'
+        this.elements.bars.high.style.width = '0px'
 
-		remove_class(document.getElementById('color_map_container'), ' hidden');
-	}
+        this.elements.labels.low.style.marginLeft = '0px'
+        this.elements.labels.high.style.marginRight = '0px'
 
-	update_hover(ind, t){
-		if(ind < 0){
-			add_class(this.elements.hover.section, ' hidden');
-		}
-		else{
-			remove_class(this.elements.hover.section, ' hidden');
+        remove_class(document.getElementById('color_map_container'), ' hidden')
+    }
 
-			this.elements.hover.label.innerHTML = this.data[t][ind].toFixed(2);
-			let pos = map(this.data[t][ind], this.values.min, this.values.max, 0, this.elements.hover.section.clientWidth);
-			this.elements.hover.line.style.marginLeft = (pos - this.elements.hover.line.clientWidth/2).toString() + 'px';
-			pos = pos - this.elements.hover.label.clientWidth/2;
-			pos = min(max(pos, 0), this.elements.hover.section.clientWidth - this.elements.hover.label.clientWidth);
-			this.elements.hover.label.style.marginLeft = pos.toString() + 'px';
-		}
-	}
+    update_hover (ind, t) {
+        if (ind < 0) {
+            add_class(this.elements.hover.section, ' hidden')
+        } else {
+            remove_class(this.elements.hover.section, ' hidden')
 
-	mouseup(){
-		if(this.dragging.left || this.dragging.right){
-			remove_class(this.elements.button, ' hidden');
-		}
-		this.dragging.left = false;
-		this.dragging.right = false;
-	}
+            this.elements.hover.label.innerHTML = this.data[t][ind].toFixed(2)
+            let pos = map(this.data[t][ind], this.values.min, this.values.max, 0, this.elements.hover.section.clientWidth)
+            this.elements.hover.line.style.marginLeft = (pos - this.elements.hover.line.clientWidth / 2).toString() + 'px'
+            pos = pos - this.elements.hover.label.clientWidth / 2
+            pos = min(max(pos, 0), this.elements.hover.section.clientWidth - this.elements.hover.label.clientWidth)
+            this.elements.hover.label.style.marginLeft = pos.toString() + 'px'
+        }
+    }
 
-	mousemove(e){
-		if(this.dragging.left){
-			let body_rect = this.elements.bar.getBoundingClientRect();
-			let right_rect = this.elements.bars.high.getBoundingClientRect();
+    mouseup () {
+        if (this.dragging.left || this.dragging.right) {
+            remove_class(this.elements.button, ' hidden')
+        }
+        this.dragging.left = false
+        this.dragging.right = false
+    }
 
-			let low_width = min(max(e.clientX - body_rect.left, 0), right_rect.left - body_rect.left);
-			this.elements.bars.low.style.width = low_width.toString() + 'px';
-			this.elements.bars.gradient.style.width = (body_rect.width - right_rect.width - low_width).toString() + 'px';
+    mousemove (e) {
+        if (this.dragging.left) {
+            const body_rect = this.elements.bar.getBoundingClientRect()
+            const right_rect = this.elements.bars.high.getBoundingClientRect()
 
-			this.values.low = (low_width/body_rect.width)*(this.values.max - this.values.min) + this.values.min;
-			this.elements.labels.low.innerHTML = this.values.low.toFixed(2);
+            const low_width = min(max(e.clientX - body_rect.left, 0), right_rect.left - body_rect.left)
+            this.elements.bars.low.style.width = low_width.toString() + 'px'
+            this.elements.bars.gradient.style.width = (body_rect.width - right_rect.width - low_width).toString() + 'px'
 
-			let low_label_rect = this.elements.labels.low.getBoundingClientRect();
-			let high_label_rect = this.elements.labels.high.getBoundingClientRect();
-			let label_left = min(max(low_width - low_label_rect.width/2, 0), low_label_rect.right - body_rect.left + (high_label_rect.left - low_label_rect.right)/2 - low_label_rect.width - 2.5);
-			this.elements.labels.low.style.marginLeft = label_left.toString() + 'px';
-			let label_right = min(max(right_rect.width - high_label_rect.width/2, 0), body_rect.right - high_label_rect.left + (high_label_rect.left - low_label_rect.right)/2 - high_label_rect.width - 2.5);
-			this.elements.labels.high.style.marginRight = label_right.toString() + 'px';
-		}
-		else if(this.dragging.right){
-			let body_rect = this.elements.bar.getBoundingClientRect();
-			let left_rect = this.elements.bars.low.getBoundingClientRect();
+            this.values.low = (low_width / body_rect.width) * (this.values.max - this.values.min) + this.values.min
+            this.elements.labels.low.innerHTML = this.values.low.toFixed(2)
 
-			let high_width = min(max(body_rect.right - e.clientX, 0), body_rect.right - left_rect.right);
-			this.elements.bars.high.style.width = high_width.toString() + 'px';
-			this.elements.bars.gradient.style.width = (body_rect.width - left_rect.width - high_width).toString() + 'px';
+            const low_label_rect = this.elements.labels.low.getBoundingClientRect()
+            const high_label_rect = this.elements.labels.high.getBoundingClientRect()
+            const label_left = min(max(low_width - low_label_rect.width / 2, 0), low_label_rect.right - body_rect.left + (high_label_rect.left - low_label_rect.right) / 2 - low_label_rect.width - 2.5)
+            this.elements.labels.low.style.marginLeft = label_left.toString() + 'px'
+            const label_right = min(max(right_rect.width - high_label_rect.width / 2, 0), body_rect.right - high_label_rect.left + (high_label_rect.left - low_label_rect.right) / 2 - high_label_rect.width - 2.5)
+            this.elements.labels.high.style.marginRight = label_right.toString() + 'px'
+        } else if (this.dragging.right) {
+            const body_rect = this.elements.bar.getBoundingClientRect()
+            const left_rect = this.elements.bars.low.getBoundingClientRect()
 
-			this.values.high = (1 - high_width/body_rect.width)*(this.values.max - this.values.min) + this.values.min;
-			this.elements.labels.high.innerHTML = this.values.high.toFixed(2);
+            const high_width = min(max(body_rect.right - e.clientX, 0), body_rect.right - left_rect.right)
+            this.elements.bars.high.style.width = high_width.toString() + 'px'
+            this.elements.bars.gradient.style.width = (body_rect.width - left_rect.width - high_width).toString() + 'px'
 
-			let low_label_rect = this.elements.labels.low.getBoundingClientRect();
-			let high_label_rect = this.elements.labels.high.getBoundingClientRect();
-			let label_right = min(max(high_width - high_label_rect.width/2, 0), body_rect.right - high_label_rect.left + (high_label_rect.left - low_label_rect.right)/2 - high_label_rect.width - 2.5);
-			this.elements.labels.high.style.marginRight = label_right.toString() + 'px';
-			let label_left = min(max(left_rect.width - low_label_rect.width/2, 0), low_label_rect.right - body_rect.left + (high_label_rect.left - low_label_rect.right)/2 - low_label_rect.width - 2.5);
-			this.elements.labels.low.style.marginLeft = label_left.toString() + 'px';
-		}
-	}
+            this.values.high = (1 - high_width / body_rect.width) * (this.values.max - this.values.min) + this.values.min
+            this.elements.labels.high.innerHTML = this.values.high.toFixed(2)
 
-	mousedown(l, r){
-		if(l)
-			this.dragging.left = true;
-		if(r)
-			this.dragging.right = true;
-	}
+            const low_label_rect = this.elements.labels.low.getBoundingClientRect()
+            const high_label_rect = this.elements.labels.high.getBoundingClientRect()
+            const label_right = min(max(high_width - high_label_rect.width / 2, 0), body_rect.right - high_label_rect.left + (high_label_rect.left - low_label_rect.right) / 2 - high_label_rect.width - 2.5)
+            this.elements.labels.high.style.marginRight = label_right.toString() + 'px'
+            const label_left = min(max(left_rect.width - low_label_rect.width / 2, 0), low_label_rect.right - body_rect.left + (high_label_rect.left - low_label_rect.right) / 2 - low_label_rect.width - 2.5)
+            this.elements.labels.low.style.marginLeft = label_left.toString() + 'px'
+        }
+    }
 
-	color_map(t, g){
-		if(this.data.length == 1)
-			t = 1;
-		else if(t > this.data.length)
-			return;
-		return this.map.map(this.data[t][g], this.values.low, this.values.high);
-	}
+    mousedown (l, r) {
+        if (l) {
+            this.dragging.left = true
+        }
+        if (r) {
+            this.dragging.right = true
+        }
+    }
+
+    color_map (t, g) {
+        if (this.data.length === 1) {
+            t = 1
+        } else if (t > this.data.length) {
+            return
+        }
+        return this.map.map(this.data[t][g], this.values.low, this.values.high)
+    }
 }
 
-function make_color_mapper(css_def){
-	let mui = new ColorMapSlider(css_def);
+function make_color_mapper (css_def) {
+    const mui = new ColorMapSlider(css_def)
 
-	mui.elements.handles.low.onmousedown = function(){
-		mui.mousedown(true, false);
-	}
+    mui.elements.handles.low.onmousedown = function () {
+        mui.mousedown(true, false)
+    }
 
-	mui.elements.handles.high.onmousedown = function(){
-		mui.mousedown(false, true);
-	}
+    mui.elements.handles.high.onmousedown = function () {
+        mui.mousedown(false, true)
+    }
 
-	mui.elements.bars.low.onmousedown = function(){
-		mui.mousedown(true, false);
-	}
+    mui.elements.bars.low.onmousedown = function () {
+        mui.mousedown(true, false)
+    }
 
-	mui.elements.bars.high.onmousedown = function(){
-		mui.mousedown(false, true);
-	}
+    mui.elements.bars.high.onmousedown = function () {
+        mui.mousedown(false, true)
+    }
 
-	return mui;
+    return mui
 }
-
-
-
-
-
-
-
