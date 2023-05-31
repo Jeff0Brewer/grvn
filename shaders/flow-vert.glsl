123ab933014e53f7e4463a6758f4ec9704656ac6
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-05-31 13:30:37 -0600

    use single flow visibility buffer, add timestep uniform

--- shaders/flow-vert.glsl
+++ shaders/flow-vert.glsl
@@ -5,12 +5,13 @@ attribute float a_Visibility;
 uniform mat4 u_ModelMatrix;
 uniform mat4 u_ViewMatrix;
 uniform mat4 u_ProjMatrix;
+uniform float u_TimeStep;
 
 varying vec4 v_Color;
 varying float v_Visibility;
 
 void main() {
     v_Color = a_Color / 255.0;
-    v_Visibility = a_Visibility;
+    v_Visibility = clamp(a_Visibility - u_TimeStep, 0.0, 1.0);
     gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
 }
--- src/ribbon_flow.js
+++ src/ribbon_flow.js
@@ -18,10 +18,13 @@ class RibbonFlow {
         const mr = 2
         const cmap = 1
 
-        this.position_buffer = new Float32Array((num_t * num_g + num_g * 2) * 2 * this.p_fpv)
-        this.color_buffer = new Uint8Array((num_t * num_g + num_g * 2) * 2 * this.c_fpv)
+        const num_vert = (num_t * num_g + num_g * 2) * 2
+        this.position_buffer = new Float32Array(num_vert * this.p_fpv)
+        this.color_buffer = new Uint8Array(num_vert * this.c_fpv)
+        this.visibility_buffer = new Uint16Array(num_vert * this.v_fpv)
         let pos_ind = 0
         let col_ind = 0
+        let vis_ind = 0
 
         for (let g = 0; g < num_g; g++) {
             let total_rotation = 0
@@ -51,18 +54,15 @@ class RibbonFlow {
             for (let pc = 0; pc < this.c_fpv * 2; pc++, col_ind++) {
                 this.color_buffer[col_ind] = 0
             }
+            for (let pv = 0; pv < this.v_fpv * 2; pv++, vis_ind++) {
+                this.visibility_buffer[vis_ind] = this.num_t
+            }
 
             for (let t = 0; t < num_t; t++) {
                 total_rotation += rotations[t][g]
 
                 let mapper = total_rotation
                 if (cmap == 1) { mapper = rotations[t][g] }
-
-                const col = [
-                    pow_map(mapper, 0, mr, lc[0], hc[0], cr),
-                    pow_map(mapper, 0, mr, lc[1], hc[1], cr),
-                    pow_map(mapper, 0, mr, lc[2], hc[2], cr)
-                ]
 
                 for (let pp = 0; pp < this.p_fpv; pp++) {
                     this_pos[pp] = positions[t][g][pp]
@@ -86,18 +86,20 @@ class RibbonFlow {
                     this.position_buffer[pos_ind] = positions[t][g][p] - ribbon_vec[p] * ribbon_size
                 }
 
-                const op = Math.pow((14 - magnitude(this_pll)) / 14, 10) * 255
+                const col = [
+                    pow_map(mapper, 0, mr, lc[0], hc[0], cr),
+                    pow_map(mapper, 0, mr, lc[1], hc[1], cr),
+                    pow_map(mapper, 0, mr, lc[2], hc[2], cr),
+                    Math.pow((14 - magnitude(this_pll)) / 14, 10)
+                ]
 
-                for (let c = 0; c < this.c_fpv - 1; c++, col_ind++) {
-                    this.color_buffer[col_ind] = col[c] * 255
+                for (let c = 0; c < this.c_fpv * 2; c++, col_ind++) {
+                    this.color_buffer[col_ind] = col[c % this.c_fpv] * 255
                 }
-                this.color_buffer[col_ind] = op
-                col_ind++
-                for (let c = 0; c < this.c_fpv - 1; c++, col_ind++) {
-                    this.color_buffer[col_ind] = col[c] * 255
+
+                for (let v = 0; v < this.v_fpv * 2; v++, vis_ind++) {
+                    this.visibility_buffer[vis_ind] = t
                 }
-                this.color_buffer[col_ind] = op
-                col_ind++
 
                 for (let pp = 0; pp < this.p_fpv; pp++) {
                     last_pos[pp] = this_pos[pp]
@@ -111,19 +113,8 @@ class RibbonFlow {
             for (let pc = 0; pc < this.c_fpv * 2; pc++, col_ind++) {
                 this.color_buffer[col_ind] = 0
             }
-        }
-
-        this.visibility_buffers = []
-        const ribbon_len = (num_t + 2) * 2
-        for (let t = 0; t < num_t; t++) {
-            this.visibility_buffers.push(new Uint8Array(this.position_buffer.length / this.p_fpv * this.v_fpv))
-            let ribbon_ind = 0
-            for (let v = 0; v < this.visibility_buffers[t].length; v++, ribbon_ind = (ribbon_ind + 1) % ribbon_len) {
-                if (ribbon_ind - 2 <= t * 2) {
-                    this.visibility_buffers[t][v] = 0
-                } else {
-                    this.visibility_buffers[t][v] = 1
-                }
+            for (let pv = 0; pv < this.v_fpv * 2; pv++, vis_ind++) {
+                this.visibility_buffer[vis_ind] = this.num_t
             }
         }
     }
@@ -154,14 +145,15 @@ class RibbonFlow {
             gl,
             'a_Visibility',
             this.v_fpv,
-            this.visibility_buffers[0],
-            gl.UNSIGNED_BYTE,
-            gl.DYNAMIC_DRAW
+            this.visibility_buffer,
+            gl.UNSIGNED_SHORT,
+            gl.STATIC_DRAW
         )
 
         this.u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
         this.u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix')
         this.u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix')
+        this.u_TimeStep = gl.getUniformLocation(gl.program, 'u_TimeStep')
     }
 
     draw (gl, modelMatrix, viewMatrix, projMatrix, timestep, rx, rz, viewport) {
@@ -175,7 +167,7 @@ class RibbonFlow {
         this.bindCol(gl)
         this.bindVis(gl)
         if (this.buffer_changed) {
-            gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.visibility_buffers[timestep])
+            gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.visibility_buffer)
         }
 
         this.buffer_changed = false
@@ -188,6 +180,7 @@ class RibbonFlow {
         model.rotate(rz, 0, 0, 1)
         model.translate(0, 0, -800)
 
+        gl.uniform1f(this.u_TimeStep, timestep)
         gl.uniformMatrix4fv(this.u_ModelMatrix, false, model.elements)
         gl.uniformMatrix4fv(this.u_ViewMatrix, false, viewMatrix.elements)
         gl.uniformMatrix4fv(this.u_ProjMatrix, false, projMatrix.elements)
@@ -197,7 +190,7 @@ class RibbonFlow {
     }
 
     slice (planefilters) {
-        for (let v = 0; v < this.visibility_buffers[0].length; v++) {
+        for (let v = 0; v < this.visibility_buffer.length; v++) {
             const pos_ind = v / this.v_fpv * this.p_fpv
             const pos = [
                 this.position_buffer[pos_ind],
@@ -209,14 +202,14 @@ class RibbonFlow {
                 outside = planefilters[f].check(pos)
             }
             if (outside) {
-                for (let t = 0; t < this.num_t; t++) { this.visibility_buffers[t][v] += 1 }
+                this.visibility_buffer[v] += this.num_t
             }
         }
         this.buffer_changed = true
     }
 
     unslice (planefilters) {
-        for (let v = 0; v < this.visibility_buffers[0].length; v++) {
+        for (let v = 0; v < this.visibility_buffer.length; v++) {
             const pos_ind = v / this.v_fpv * this.p_fpv
             const pos = [
                 this.position_buffer[pos_ind],
@@ -228,23 +221,9 @@ class RibbonFlow {
                 outside = planefilters[f].check(pos)
             }
             if (outside) {
-                for (let t = 0; t < this.num_t; t++) { this.visibility_buffers[t][v] -= 1 }
+                this.visibility_buffer[v] -= this.num_t
             }
         }
         this.buffer_changed = true
-    }
-
-    reset_slices () {
-        const ribbon_len = (this.num_t + 2) * 2
-        for (let t = 0; t < this.num_t; t++) {
-            let ribbon_ind = 0
-            for (let v = 0; v < this.visibility_buffers[t].length; v++, ribbon_ind = (ribbon_ind + 1) % ribbon_len) {
-                if (ribbon_ind - 2 <= t * 2) {
-                    this.visibility_buffers[t][v] = 0
-                } else {
-                    this.visibility_buffers[t][v] = 1
-                }
-            }
-        }
     }
 }
