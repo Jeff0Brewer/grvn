b08e426dac2b8cc6f2feac1705dbf1a02614239e
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-10-19 14:11:43 -0600

    reorganize / rename snake case vars

--- public/lib/color_map.js
+++ public/lib/color_map.js
@@ -1,36 +1,33 @@
 class ColorMap {
-    constructor (css_def) {
+    constructor (cssGradient) {
         this.steps = []
         this.colors = []
 
-        let curr_ind = 0
+        let currInd = 0
         while (true) {
-            const a = css_def.indexOf('#', curr_ind)
-            const b = css_def.indexOf(' ', curr_ind)
-            const c = css_def.indexOf('%', curr_ind)
-            curr_ind = c + 3
-            if (a < 0 || b < 0 || c < 0) { break }
-            this.colors.push(hex_to_rgb(css_def.substring(a, b)))
-            this.steps.push(parseFloat(css_def.substring(b + 1, c)) / 100)
+            const a = cssGradient.indexOf('#', currInd)
+            const b = cssGradient.indexOf(' ', currInd)
+            const c = cssGradient.indexOf('%', currInd)
+            currInd = c + 3
+            if (a < 0 || b < 0 || c < 0) {
+                break
+            }
+
+            this.colors.push(hex_to_rgb(cssGradient.substring(a, b)))
+            this.steps.push(parseFloat(cssGradient.substring(b + 1, c)) / 100)
         }
     }
 
     map (value, low, high) {
-        if (value <= low) {
-            return this.colors[0]
-        }
-        if (value >= high) {
-            return this.colors[this.colors.length - 1]
-        }
+        if (value <= low) { return this.colors[0] }
+        if (value >= high) { return this.colors[this.colors.length - 1] }
 
-        const percentage = (value - low) / (high - low)
         let i = 0
+        const percentage = (value - low) / (high - low)
         while (i + 1 < this.steps.length && this.steps[i + 1] < percentage) {
             i++
         }
-        if (i >= this.steps.length - 1) {
-            return this.colors[i]
-        }
+        if (i === this.steps.length - 1) { return this.colors[i] }
 
         const mid = (percentage - this.steps[i]) / (this.steps[i + 1] - this.steps[i])
         const [r0, g0, b0] = this.colors[i]
