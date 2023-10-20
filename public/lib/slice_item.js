141d0f8543e5adc5a40549532357c70579590105
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-10-20 12:23:53 -0600

    remove slice template from html, construct in js

--- public/lib/slice_item.js
+++ public/lib/slice_item.js
@@ -1,56 +1,52 @@
 class SliceItem {
     constructor (ind, planefilters, interface_out) {
         this.planefilters = planefilters
-        this.ind = ind
         this.removed = false
 
-        const insert = document.getElementById('slice_proto')
-        const source = insert.childNodes[1]
-        const dest = document.getElementById('slice_list')
-        const cloned = source.cloneNode(true)
-        dest.insertBefore(cloned, insert)
+        this.dom = document.createElement('div')
+        const canvas = document.createElement('canvas')
+        const label = document.createElement('p')
+        const deleteButton = document.createElement('button')
+        this.dom.className = 'list_item selection_highlight'
+        canvas.className = 'item_canvas'
+        label.className = 'item_label'
+        deleteButton.className = 'item_delete'
+        this.dom.append(canvas, label, deleteButton)
+        document.getElementById('slice_list').appendChild(this.dom)
 
-        this.elements = {
-            body: cloned,
-            canvas: cloned.childNodes[1],
-            ind: cloned.childNodes[3],
-            delete: cloned.childNodes[5]
-        }
+        label.innerHTML = `Slice ${ind}`
+        deleteButton.onmouseup = () => { this.delete() }
 
-        this.elements.canvas.width = 20 * window.devicePixelRatio
-        this.elements.canvas.height = 20 * window.devicePixelRatio
-        const ctx = this.elements.canvas.getContext('2d')
+        canvas.width = 20 * window.devicePixelRatio
+        canvas.height = 20 * window.devicePixelRatio
+        const ctx = canvas.getContext('2d')
         ctx.lineWidth = 1
         ctx.strokeStyle = 'rgb(189,189,189)'
 
         const vp = interface_out[1]
         const points = interface_out[0]
         const scale = max(vp.width, vp.height)
-        const x_margin = map(scale - vp.width, 0, scale, 0, this.elements.canvas.width)
-        const y_margin = map(scale - vp.height, 0, scale, 0, this.elements.canvas.height)
+        const x_margin = map(scale - vp.width, 0, scale, 0, canvas.width)
+        const y_margin = map(scale - vp.height, 0, scale, 0, canvas.height)
 
         for (let i = 0; i < points.length; i++) {
             for (let j = 0; j < 2; j++) {
-                points[i][j][0] = map(points[i][j][0] - vp.x, 0, scale, x_margin, this.elements.canvas.width - x_margin)
-                points[i][j][1] = map(points[i][j][1] - vp.y, 0, scale, this.elements.canvas.height - y_margin, y_margin)
+                points[i][j][0] = map(points[i][j][0] - vp.x, 0, scale, x_margin, canvas.width - x_margin)
+                points[i][j][1] = map(points[i][j][1] - vp.y, 0, scale, canvas.height - y_margin, y_margin)
             }
             const slope = (points[i][1][1] - points[i][0][1]) / (points[i][1][0] - points[i][0][0])
-            const p0 = [points[i][0][0] + this.elements.canvas.width, points[i][0][1] + slope * this.elements.canvas.width]
-            const p1 = [points[i][0][0] - this.elements.canvas.width, points[i][0][1] - slope * this.elements.canvas.width]
+            const p0 = [points[i][0][0] + canvas.width, points[i][0][1] + slope * canvas.width]
+            const p1 = [points[i][0][0] - canvas.width, points[i][0][1] - slope * canvas.width]
 
             ctx.beginPath()
             ctx.moveTo(p0[0], p0[1])
             ctx.lineTo(p1[0], p1[1])
             ctx.stroke()
         }
-
-        this.elements.ind.innerHTML += ind.toString()
-
-        this.elements.delete.onmouseup = () => { this.delete() }
     }
 
     delete () {
-        this.elements.body.remove()
+        this.dom.remove()
         this.removed = true
     }
 }
